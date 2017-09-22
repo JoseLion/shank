42f1d5a90a388edd55270c6a3f06b92c76ff48ce
Christian <chris.mood92@gmail.com>
2017-09-22 13:29:35 -0500

    splash screen

--- app/src/components/screens/SplashScreen/SplashScreen.js
+++ app/src/components/screens/SplashScreen/SplashScreen.js
@@ -4,17 +4,29 @@ import {Button, StyleSheet, Text, View, Image, StatusBar} from 'react-native';
 import MainStyles from '../../../styles/main';
 import LocalStyles from './styles/local';
 
-const SplashScreen = ({navigation}) => (
-    <View style={LocalStyles.container}>
-        <StatusBar hidden={true} />
-        <Image
-            source={require('../../../../resources/shankLogo/IOS/regular/shankLogo.png')}
-            style={MainStyles.iconLG}/>
-        <Button
-            onPress={() => navigation.dispatch({type: 'Splash'})}
-            title="Go to Log in"/>
-    </View>
-);
+const SplashScreen = ({navigation}) => {
+    componentWillMount = () => {
+        console.log("Si entra mmvga")
+        setTimeout(() => {
+            navigation.dispatch({type: 'Splash'});
+        }, 500);
+    };
+
+    let component = componentWillMount();
+    return (
+
+        <View style={LocalStyles.container}>
+            {component}
+            <StatusBar hidden={true}/>
+            <Image
+                source={require('../../../../resources/shankLogo/IOS/regular/shankLogo.png')}
+                style={MainStyles.iconLG}/>
+            <Button
+                onPress={() => navigation.dispatch({type: 'Splash'})}
+                title="Go to Log in"/>
+        </View>
+    );
+}
 
 SplashScreen.propTypes = {
     navigation: PropTypes.object.isRequired,
