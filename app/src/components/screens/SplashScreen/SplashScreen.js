65022b6363d28edfd3bf2657a47150e9c1187a9a
chri5bot <chris.mood92@gmail.com>
2017-09-24 13:50:55 -0500

    SplashScreen format

--- app/src/components/screens/SplashScreen/SplashScreen.js
+++ app/src/components/screens/SplashScreen/SplashScreen.js
@@ -7,47 +7,56 @@ import LocalStyles from './styles/local';
 const logoTrans = require('../../../../resources/shankLogo/IOS/trans/shankLogoTrans.png');
 const logoRegular = require('../../../../resources/shankLogo/IOS/regular/shankLogo.png');
 
-class Greeting extends Component {
+let randomHex = () => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+};
+
+export default class SplashScreen extends Component {
+
+    static propTypes = {
+        navigation: PropTypes.object.isRequired,
+    };
+
+    static navigationOptions = {
+        title: 'Splash',
+        header: null
+    };
+
     constructor(props) {
         super(props);
-        this.state = {showImages: true};
-        // Toggle the state every second
+        this.state = {
+            showImages: true,
+            backgroundColor: randomHex()
+        };
+
         setInterval(() => {
             this.setState(previousState => {
-                return {showImages: !previousState.showImages};
+                return {
+                    showImages: !previousState.showImages,
+                    backgroundColor: randomHex(),
+                };
             });
         }, 1000);
     }
 
     render() {
+        let navigation = this.props.navigation;
         let imgSource = this.state.showImages ? logoTrans : logoRegular;
         return (
-            <Image source={imgSource}
-                   style={MainStyles.iconXLG}/>
+            <View style={[LocalStyles.container, {backgroundColor: this.state.backgroundColor}]}>
+                <StatusBar hidden={true}/>
+                <Image source={imgSource}
+                       style={MainStyles.iconXLG}/>
+                <Button
+                    onPress={() => navigation.dispatch({type: 'Splash'})}
+                    title="Go to Log in"/>
+            </View>
+
         );
     }
 }
-
-const SplashScreen = ({navigation}) => {
-    return (
-        <View style={LocalStyles.container}>
-            <StatusBar hidden={true}/>
-            <Greeting/>
-            <Button
-                onPress={() => navigation.dispatch({type: 'Splash'})}
-                title="Go to Log in"/>
-        </View>
-    );
-};
-
-SplashScreen.propTypes = {
-    navigation: PropTypes.object.isRequired,
-};
-
-SplashScreen.navigationOptions = {
-    title: 'Splash',
-    header: null
-};
-
-
-export default SplashScreen;
