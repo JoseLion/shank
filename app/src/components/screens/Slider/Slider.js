39c82ff990957cb8fc0a39e6463c3628db84ceda
Gavilanes <kamag89@gmail.com>
2017-09-25 12:39:08 -0500

    version control

--- app/src/components/screens/Slider/Slider.js
+++ app/src/components/screens/Slider/Slider.js
@@ -24,27 +24,6 @@ export default class SliderScreen extends Component {
         let navigation = this.props.navigation;
         return (
             <Swiper style={LocalStyles.wrapper} showsButtons={false}>
-<<<<<<< HEAD
-                <View style={LocalStyles.slide1}>
-                    <Image style={LocalStyles.coverImage}
-                           source={require('../../../../resources/initSlider/IOS/slider/init1/sliderInit1.png')}/>
-                </View>
-                <View style={LocalStyles.slide2}>
-                    <Image style={LocalStyles.coverImage}
-                           source={require('../../../../resources/initSlider/IOS/slider/init2/sliderInit2.png')}/>
-                </View>
-                <View style={LocalStyles.slide3}>
-                    <Image style={LocalStyles.coverImage}
-                           source={require('../../../../resources/initSlider/IOS/slider/init3/sliterInit3.png')}>
-                    </Image>
-                    <Text style={MainStyles.shankTitle}>SHANK</Text>
-                    <Text style={MainStyles.medShankFont}>GET YOUR REWARD</Text>
-                    <Text style={MainStyles.smallShankFont}>PROFIT FROM THE OUTCOME AND BET AGAIN</Text>
-                    <Button style={LocalStyles.startButton}
-                            onPress={() => navigation.dispatch({type: 'Slider'})}
-                            title="Let's get started"/>
-
-=======
                 <View style={LocalStyles.slide}>
                     <Image style={LocalStyles.coverImage}
                            source={require('../../../../resources/initSlider/IOS/slider/init1/sliderInit1.png')}/>
@@ -66,7 +45,6 @@ export default class SliderScreen extends Component {
                                 onPress={() => navigation.dispatch({type: 'Slider'})}
                                 title="Let's get start"/>
                     </Image>
->>>>>>> 26ce4dee874c04f3520d3646484829b3a7788183
                 </View>
             </Swiper>
         );
