a3290ce9d14c76870bd82420017dbd70c2d9b58b
chri5bot <chris.mood92@gmail.com>
2017-09-28 21:30:10 -0500

    merge

--- app/src/components/screens/Register/Register.js
+++ app/src/components/screens/Register/Register.js
@@ -4,13 +4,23 @@
 
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {AsyncStorage, Button, StyleSheet, Text, View, TextInput, TouchableHighlight} from 'react-native';
+import {
+    AsyncStorage,
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableHighlight,
+    TouchableOpacity
+} from 'react-native';
 import MainStyles from '../../../styles/main';
 import LocalStyles from './styles/local'
 import Notifier from '../../../core/Notifier';
 import NoAuthModel from '../../../core/NoAuthModel';
 import * as Constants from '../../../core/Constans';
 import Spinner from 'react-native-loading-spinner-overlay';
+
 
 export default class Register extends Component {
 
@@ -27,6 +37,7 @@ export default class Register extends Component {
     constructor(props) {
         super(props);
 
+        this._register = this._register.bind(this);
         this._handleNewRegistry = this._handleNewRegistry.bind(this);
 
         this.state = {
@@ -42,18 +53,36 @@ export default class Register extends Component {
         this.setState({loading: loading});
     }
 
+    _register() {
+        const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync('<APP_ID>', {
+            permissions: ['public_profile'],
+        });
+        if (type === 'success') {
+            // Get the user's name using Facebook's Graph API
+            const response = await
+            fetch(
+                `https://graph.facebook.com/me?access_token=${token}`);
+            Alert.alert(
+                'Logged in!',
+                `Hi ${(await response.json()).name}!`,
+            );
+        }
+    }
+
     async _registerUserAsync(data) {
         this.setLoading(true);
         NoAuthModel.create('register', data).then((response) => {
             AsyncStorage.setItem(Constants.AUTH_TOKEN, response.token, () => {
                 AsyncStorage.setItem(Constants.USER_PROFILE, JSON.stringify(response.user), () => {
                     this.setLoading(false);
-                    this.props.navigation.dispatch({type: 'Login'})
+                    this.props.navigation.dispatch({type: 'Login'});
+                    console.log("error--->", response)
                 });
             });
         }).catch((error) => {
             this.setLoading(false);
             setTimeout(() => {
+                console.log("error--->", error);
                 Notifier.message({title: 'ERROR', message: error});
             }, 2000);
         });
@@ -135,6 +164,11 @@ export default class Register extends Component {
                     onPress={this._handleNewRegistry}
                     style={MainStyles.goldenShankButton}>
                     <Text style={LocalStyles.buttonText}>Register</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    onPress={this._register()}
+                    style={MainStyles.goldenShankButton}>
+                    <Text style={LocalStyles.buttonText}>Register with Facebook</Text>
                 </TouchableHighlight>
             </View>
         );
--- app/src/config/variables.js
+++ app/src/config/variables.js
@@ -8,12 +8,12 @@
 
 
 // Prod
-const Host = 'http://192.168.1.3:3000/';
+const Host = 'http://192.168.1.7:3000/';
 const ApiHost = Host + 'api/';
 
 const AuthToken = 'wwfAuthToken';
 const ApiKey = 'MTU2NDJkYzcxZjkwNTk4NjdlNjVlYWRjZTI5Zjdl';
-const AssetsURL = 'http://192.168.1.3:3000/uploads';
+const AssetsURL = 'http://192.168.1.7:3000/uploads';
 const Timeout = 20000;
 const version = '1.2';
 
