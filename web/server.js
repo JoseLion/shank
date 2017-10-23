4a865b99b2392166216e168dbbf518af90a7aad1
MnMistake <MnMistake>
2017-10-22 22:22:54 -0500

    added lte admin working

--- web/server.js
+++ web/server.js
@@ -1,23 +1,43 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+var http = require("http");  //1
+var url = require("url");  //2
+var express = require("express");
+var consolidate = require('consolidate');
+var handlebars = require('handlebars');
+var bodyParser = require('body-parser');
 
-const express = require('express');
-const next = require('next');
+var routes = require('./routes');
 
-const PORT = process.env.PORT;
+var app = express();
 
-const app = next({ dev: true });
-const handle = app.getRequestHandler();
+app.set('views', 'views'); //Set the folder-name from where you serve the html page. ]
+app.set('view engine', 'html');
+app.engine('html', consolidate.handlebars);
+app.use(express.static('./public')); //Set the folder from where you serve all static files like images, css, javascripts, libraries etc
+app.use(bodyParser.urlencoded({ extended: true }));
+var portNumber = process.argv[2] || 8000;
 
-app.prepare()
-  .then(() => {
-    const server = express();
+http.createServer(app).listen(portNumber, function(){
+	console.log('Server listening at port '+ portNumber);
+	routes.initialize(app);
+});
 
-    // Use next to render pages on all other routes
-    server.get('*', (req, res) => handle(req, res));
-    
-    server.listen(PORT, () => {
-      console.log(`Server listening on port ${PORT}`);
-    });
-  });
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var config = require('./webpack.config');
+
+// new WebpackDevServer(webpack(config), {
+// 	publicPath: config.output.publicPath,
+// 	hot: true,
+// 	historyApiFallback: true
+// }).listen(3000, 'localhost', function (err, result) {
+// 	if (err) {
+// 		console.log(err);
+// 	}
+
+//   	console.log('Listening at localhost:3000');
+// });
+
+/*
+
+
+*/
