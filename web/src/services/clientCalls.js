ef694ab1c1d8863f52072a0130842b7f7f5dde08
mnmistake123 <mnmistake123@gmail.com>
2017-10-29 12:45:23 -0500

    login

--- web/src/services/clientCalls.js
+++ web/src/services/clientCalls.js
@@ -9,129 +9,132 @@ let internetError = 'No internet connection available.';
 let requestServerError = 'We couldn\'t connect to the server. Please try later';
 let parsingResponseError = 'Error getting server response.';
 
-define(
-    [],
-    async function () {
-        exports.get = async function(resource){
-            let options = {
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                }
-            };
-    
-            const response = await fetch(ApiHost + resource, options).catch(
-                error => {
-                    throw requestServerError;
-                }
-            );
-    
-            const json = await response.json().catch(
-                error => {
-                    throw parsingResponseError;
-                }
-            );
-    
-            if (json.error !== '') {
-                throw json.error;
-            }
-            else {
-                return json.response;
-            }
-        };
-        exports.create = async function(resource,params){
-            const data = JSON.stringify(params);
-            let options = {
-                method: 'POST',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: data
-            };
-    
-            const response = await fetch(ApiHost + resource, options).catch(
-                error => {
-                    throw requestServerError;
-                }
-            );
-    
-            const json = await response.json().catch(
-                error => {
-                    throw parsingResponseError;
-                }
-            );
-    
-            if (json.error !== '') {
-                throw json.error;
-            }
-            else {
-                return json.response;
-            }
-        };
 
-        exports.update = async function(resource,params){
-            const data = JSON.stringify(params);
-            
-            let options = {
-                method: 'PUT',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                body: data
-            }
-    
-            const response = await fetch(ApiHost + resource, options).catch(
-                error => {
-                    throw requestServerError;
+module.exports = {
+    async get(resource) {
+        
+                let options = {
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                };
+        
+                const response = await fetch(ApiHost + resource, options).catch(
+                    error => {
+                        throw requestServerError;
+                    }
+                );
+        
+                const json = await response.json().catch(
+                    error => {
+                        throw parsingResponseError;
+                    }
+                );
+        
+                if (json.error !== '') {
+                    throw json.error;
                 }
-            );
-    
-            const json = await response.json().catch(
-                error => {
-                    throw parsingResponseError;
+                else {
+                    return json.response;
                 }
-            );
-    
-            if (json.error !== '') {
-                throw json.error;
-            }
-            else {
-                return json.response;
-            }
-        };
-
-        exports.remove = async function(resource){
-            let options = {
-                method: 'DELETE',
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
+            },
+        
+            async create(resource, params) {
+        
+                const data = JSON.stringify(params);
+        
+                let options = {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: data
+                };
+        
+                const response = await fetch(ApiHost + resource, options).catch(
+                    error => {
+                        throw requestServerError;
+                    }
+                );
+        
+                const json = await response.json().catch(
+                    error => {
+                        throw parsingResponseError;
+                    }
+                );
+        
+                if (json.error !== '') {
+                    throw json.error;
                 }
-            }
-    
-            const response = await fetch(ApiHost + resource, options).catch(
-                error => {
-                    throw requestServerError;
+                else {
+                    return json.response;
                 }
-            );
-    
-            const json = await response.json().catch(
-                error => {
-                    throw parsingResponseError;
+            },
+        
+            async update(resource, params) {
+        
+                const data = JSON.stringify(params);
+        
+                let options = {
+                    method: 'PUT',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: data
                 }
-            );
-    
-            if (json.error !== '') {
-                //return Promise.reject(new Error(json.error));
-                throw json.error;
-            }
-            else {
-                return json.response;
-            }
-        };
-       
-    }
-)
+        
+                const response = await fetch(ApiHost + resource, options).catch(
+                    error => {
+                        throw requestServerError;
+                    }
+                );
+        
+                const json = await response.json().catch(
+                    error => {
+                        throw parsingResponseError;
+                    }
+                );
+        
+                if (json.error !== '') {
+                    throw json.error;
+                }
+                else {
+                    return json.response;
+                }
+            },
+        
+            async remove(resource) {
+        
+                let options = {
+                    method: 'DELETE',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    }
+                }
+        
+                const response = await fetch(ApiHost + resource, options).catch(
+                    error => {
+                        throw requestServerError;
+                    }
+                );
+        
+                const json = await response.json().catch(
+                    error => {
+                        throw parsingResponseError;
+                    }
+                );
+        
+                if (json.error !== '') {
+                    //return Promise.reject(new Error(json.error));
+                    throw json.error;
+                }
+                else {
+                    return json.response;
+                }
+            },
+};
