133fc28a0194f66babac06fa28acb3f0295ccdaa
lfgarcia <luis.fernando.garcia.castro@gmail.com>
2017-11-23 15:41:25 -0500

    Se eliminan los arcchivos de node_ modules, - @Ajustes varios en el proyecto

--- app/src/config/variables.js
+++ app/src/config/variables.js
@@ -9,8 +9,8 @@
 // Prod
 //HOST IP DE SU DOCKER TODO://CHECK
 
-//const Host = 'http://34.197.96.93/'; testing server AWS
-//const ClienHost = '34.197.96.93:8000/';
+//const Host = 'http://104.198.224.166/'; testing server Google Cloud
+//const ClienHost = '104.198.224.166:8000/';
 
 //const Host = 'http://192.168.1.3:3000/'; testing dev RAUL
 //http://34.197.96.93/api/allUsers test api users
@@ -20,13 +20,13 @@
 
 //const ClienHost = '192.168.1.3:8000/';
 
-const Host = 'http://34.197.96.93/';
+const Host = 'http://104.198.224.166:3000/';
 const ApiHost = Host + 'api/';
-const ClienHost = '34.197.96.93:8000/';
+const ClienHost = '104.198.224.166:8000/';
 
 const AuthToken = 'shank-auth-token';
 const ApiKey = 'MTU2NDJkYzcxZjkwNTk4NjdlNjVlYWRjZTI5Zjdl';
-const AssetsURL = 'http://34.197.96.93/uploads';
+const AssetsURL = 'http://104.198.224.166:3000/uploads';
 const Timeout = 60000;
 const version = '2.9';
 
