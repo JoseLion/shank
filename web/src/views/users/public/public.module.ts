40c71a5e7a6b95107a90cdb291283bd62298f16d
lfgarcia <luis.fernando.garcia.castro@gmail.com>
2017-11-28 00:08:28 -0500

    - Sobrescritura del proyecto WEB

---
+++ web/src/views/users/public/public.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {PublicViewComponent} from "./public.component.ts";
+
+@NgModule({
+    declarations: [PublicViewComponent],
+    imports     : [BrowserModule],
+})
+
+export class PublicViewModule {}
