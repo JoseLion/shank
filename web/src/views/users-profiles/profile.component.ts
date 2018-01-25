b87cc242f823402cafff02ae11ad0bed437dbb1b
lfgarcia <luis.fernando.garcia.castro@gmail.com>
2018-01-24 21:46:13 -0500

    - Ajuste en pantallas de usuarios y perfiles

--- web/src/views/users-profiles/profile.component.ts
+++ web/src/views/users-profiles/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { consts } from 'views/core/consts';
 import { Rest } from 'views/core/rest';
+import { SweetAlert } from 'views/core/sweetAlert';
+import { MessageService } from 'primeng/components/common/messageservice';
 
 @Component({
     selector: 'profile',
@@ -8,31 +10,17 @@ import { Rest } from 'views/core/rest';
 })
 export class ProfileViewComponent {
 
-    private _profiles: any[];
-    private _isLoading: boolean = true;
+    public profiles: any[];
+    public isLoading: boolean = true;
 
-    constructor(private rest : Rest) {
-        this.rest.post('findProfiles', {})
+    constructor(private rest : Rest, private sweetAlert : SweetAlert, private messageService : MessageService) {
+        this.rest.post('profiles/findProfiles')
         .subscribe(
-            response => {
-                let res = response.json().response;
-                this.profiles = res;
-            },
-            error => {
-                console.log('ERROR CONSUMO SERVICIO: ',  error.text());
-            },
-            () => {
-                this.isLoading = false;
+            response => { this.profiles = response.json().response; },
+            error => { SweetAlert.errorNotif(error.text(), this.messageService); },
+            () => { this.isLoading = false;
             }
         );
     }
-
-    /* GETTERS AND SETTERS */
-
-    set profiles(_profiles:any[]) { this._profiles = _profiles; }
-    get profiles() { return this._profiles; }
-
-    set isLoading(_isLoading:boolean) { this._isLoading = _isLoading; }
-    get isLoading():boolean { return this._isLoading; }
 
 }
--- web/src/views/users-profiles/user.component.ts
+++ web/src/views/users-profiles/user.component.ts
@@ -11,21 +11,21 @@ import { MessageService } from 'primeng/components/common/messageservice';
 })
 export class UserViewComponent {
 
-    private _profiles: any[] = [];
-    private _users: any[] = [];
-    private _isLoading: boolean = true;
-    private _title: string;
-    private _isAdmin: boolean;
-    private _display: boolean;
-    private _user: any = {};
-    private _modalHeader: string;
-    private _search: any = {};
+    public profiles: any[] = [];
+    public users: any[] = [];
+    public isLoading: boolean = true;
+    public title: string;
+    public isAdmin: boolean;
+    public display: boolean;
+    public user: any = {};
+    public modalHeader: string;
+    public search: any = {};
 
     constructor(private rest : Rest, private route : ActivatedRoute, private sweetAlert : SweetAlert, private messageService : MessageService) {
         this.route.params.subscribe(params => {
             this.title = params['profile'];
             this.isAdmin = params['profile'] === 'admin';
-            this.search = {profile: this.title === 'admin' ? 1 : 2};
+            this.search = {profile: this.title};
             this.findUsers();
             this.rest.post('profiles/findProfiles')
             .subscribe(
@@ -43,7 +43,7 @@ export class UserViewComponent {
             error => { SweetAlert.errorNotif(error.text(), this.messageService); },
             () => { this.isLoading = false; }
         );
-    }
+    };
 
     openDialog = function(isNew, user?) {
         this.modalHeader = isNew ? 'New User' : 'Edit User';
@@ -86,34 +86,5 @@ export class UserViewComponent {
             );
         });
     };
-
-    /* GETTERS AND SETTERS */
-
-    set profiles(_profiles:any[]) { this._profiles = _profiles; }
-    get profiles() { return this._profiles; }
-
-    set users(_users:any[]) { this._users = _users; }
-    get users() { return this._users; }
-
-    set isLoading(_isLoading:boolean) { this._isLoading = _isLoading; }
-    get isLoading() { return this._isLoading; }
-
-    set title(_title:string) { this._title = _title; }
-    get title() { return this._title; }
-
-    set isAdmin(_isAdmin:boolean) { this._isAdmin = _isAdmin; }
-    get isAdmin() { return this._isAdmin; }
-
-    set display(_display:boolean) { this._display = _display; }
-    get display() { return this._display; }
-
-    set user(_user:any[]) { this._user = _user; }
-    get user() { return this._user; }
-
-    set modalHeader(_modalHeader:string) { this._modalHeader = _modalHeader; }
-    get modalHeader() { return this._modalHeader; }
-
-    set search(_search:any) { this._search = _search; }
-    get search() { return this._search; }
 
 }
