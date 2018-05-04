dc3cd29bdf1633e0f9d7301b67ab5e63c6a29a0d
Jose Luis Leon <joseluis.provedatos@gmail.com>
2018-05-04 17:54:28 -0500

    - ANdroid alpha version 1.0.0 (2)

--- app/src/config/variables.js
+++ app/src/config/variables.js
@@ -1,6 +1,6 @@
 // shank.levelaptesting.com
 // 192.168.10.152
-const endPoint = '192.168.10.152';
+const endPoint = 'shank.levelaptesting.com';
 const port = '3000';
 const Host = `http://${endPoint}:${port}/`;
 const ApiHost = Host + 'api/';
