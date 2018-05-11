375a812bbede68d94c5d7556a58a9ff5b72bb6fb
Efrain Leon <efrain.leonh.trabajo@gmail.com>
2018-05-11 15:49:23 -0500

    removed response module

--- server/modules/responses/forbidden.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (code, message, extraData) {
-    this.status(code || 403);
-    this.send({
-        response: {},
-        error: message || 'Esta solicitud está prohibida'
-    });
-};
