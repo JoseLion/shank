b791ba1ffb4e74b7c16369795deed35dc055803f
Jose Luis Leon <joseluis.provedatos@gmail.com>
2018-05-28 16:27:51 -0500

    - Fix

--- app/android/app/src/main/java/com/shank/MainApplication.java
+++ app/android/app/src/main/java/com/shank/MainApplication.java
@@ -40,7 +40,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new VectorIconsPackage(),
                     new SvgPackage(),
                     new ReactNativePushNotificationPackage(),
                     new LinearGradientPackage(),
