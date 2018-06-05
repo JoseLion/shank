865e9867b9c8204af1f0fea1a6be22d9316f3b5b
Efrain Leon <efrain.leonh.trabajo@gmail.com>
2018-06-05 15:57:08 -0500

    added landing page

--- web/public/src/app/modules/home/controllers/home.controller.js
+++ web/public/src/app/modules/home/controllers/home.controller.js
@@ -18,6 +18,12 @@
   function HomeController() {
     var vm = this;
     
-    vm.creationDate = 1526418254625;
+    vm.players = [
+      {id: 1, position: 1, player: 'Tiger Woods', total: -3, thru: 17, today: -3, points: '8,000'},
+      {id: 2, position: 'T2', player: 'Phil Mickelson', total: -1, thru: 17, today: -2, points: '7,300'},
+      {id: 3, position: 'T2', player: 'Jordan Spieth', total: -1, thru: 18, today: -2, points: '7,300'},
+      {id: 4, position: 'T2', player: 'Emiliano Grillo', total: -1, thru: 18, today: 'E', points: '7,300'},
+      {id: 5, position: 5, player: 'Patrick Rodgers', total: 'E', thru: 18, today: '+1', points: '5,550'}
+    ];
   }
 })();
