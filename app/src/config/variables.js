3207fb9b49b535c68da65fa2daefe05638e868ae
Jose Luis Leon <joseluis.provedatos@gmail.com>
2018-06-06 17:02:46 -0500

    - Host is production with static IP

--- app/src/config/variables.js
+++ app/src/config/variables.js
@@ -1,7 +1,7 @@
 // shank.levelaptesting.com
 // 192.168.10.152
-const endPoint = '192.168.10.152';
-const port = '3000';
+const endPoint = '35.199.81.172';
+const port = '4000';
 const Host = `http://${endPoint}:${port}/`; 
 const ApiHost = Host + 'api/';
 const FileHost = ApiHost + "archive/download/";
