69b6436b0fead72e8f3fe6d057d95365ef091b60
Efrain Leon <efrain.leonh.trabajo@gmail.com>
2018-06-08 17:59:17 -0500

    added footer

--- web/public/src/app/index.run.js
+++ web/public/src/app/index.run.js
@@ -6,9 +6,15 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($log, $rootScope, $state, $window) {
 
     $log.debug('runBlock end');
+    
+    $rootScope.$on("$stateChangeSuccess", function (event, currentRoute, previousRoute) {
+      $window.scrollTo(0, 0);
+    });
+    
+    $rootScope.state = $state;
   }
 
 })();
